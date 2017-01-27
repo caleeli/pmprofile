7862479e30c504ab4358d86ffff58fb85b3014d2
davidcallizaya <davidcallizaya@192.168.11.22>
2017-01-27 17:13:04 -0400

    fix

--- services/env.php
+++ services/env.php
@@ -1,6 +1,6 @@
 <?php
 $userId = '00000000000000000000000000000001';
-$task = executeQuery('select PRO_UID, TAS_UID, PRO_ID from TASK left join PROCESS on (TASK.PRO_UID=PROCESS.PRO_UID) where TAS_START="TRUE" LIMIT 1');
+$task = executeQuery('select TASK.PRO_UID, TAS_UID, PRO_ID from TASK left join PROCESS on (TASK.PRO_UID=PROCESS.PRO_UID) where TAS_START="TRUE" LIMIT 1');
 $processId = $task[1]['PRO_UID'];
 $processIdNum = $task[1]['PRO_ID'];
 $taskId = $task[1]['TAS_UID'];
