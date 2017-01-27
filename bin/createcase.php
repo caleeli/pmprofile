e044194c1080fc9cbeb264abb1a761f3350221a8
davidcallizaya <davidcallizaya@192.168.11.22>
2017-01-27 16:04:39 -0400

    fix

--- bin/createcase.php
+++ bin/createcase.php
@@ -1,32 +1,9 @@
 <?php
-$timezone = 'UTC';
-if (is_link('/etc/localtime')) {
-    // Mac OS X (and older Linuxes)
-    // /etc/localtime is a symlink to the
-    // timezone in /usr/share/zoneinfo.
-    $filename = readlink('/etc/localtime');
-    if (strpos($filename, '/usr/share/zoneinfo/') === 0) {
-        $timezone = substr($filename, 20);
-    }
-} elseif (file_exists('/etc/timezone')) {
-    // Ubuntu / Debian.
-    $data = file_get_contents('/etc/timezone');
-    if ($data) {
-        $timezone = $data;
-    }
-} elseif (file_exists('/etc/sysconfig/clock')) {
-    // RHEL / CentOS
-    $data = parse_ini_file('/etc/sysconfig/clock');
-    if (!empty($data['ZONE'])) {
-        $timezone = $data['ZONE'];
-    }
-}
-
-date_default_timezone_set($timezone);
+require './fixTimeZone.php';
 
 class CasesGenerator
 {
-    const LIMIT_INSERT_ROWS = 1;
+    const LIMIT_INSERT_ROWS = 100;
 
     public $proUid = '9388875235852ac30cbf367053242568';
     public $tasUid = '26948945158764ccf716d86083238937';
